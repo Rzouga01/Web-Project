119d6f347bcf9ea37c2715e627ce1035095f5164
AtfastrSlushyMaker <atfastr@gmail.com>
2023-11-16 23:49:21 +0100

    Refactor category_class.php for better readability and fix SQL query in category_delete.php

--- model/Category/category_class.php
+++ model/Category/category_class.php
@@ -1,157 +1,132 @@
 <?php
 
-class Category {
+class Category
+{
     private $name;
     private $description;
-    
-    public function __construct($name, $description) {
+
+    public function __construct($name, $description)
+    {
         $this->name = $name;
         $this->description = $description;
-
-
-
-}
-public function getName() {
-    return $this->name;
-}
-public function getDescription() {
-    return $this->description;
-}
-public function setName($name) {
-    $this->name = $name;
-}
-public function setDescription($description) {
-    $this->description = $description;
-}
-}
-function update (){
-
-    
-require '../Connection/connection.php';
-
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-
-    $name = $_POST['name-update'];
-    $newName = $_POST['name'];
-    $newDescription = $_POST['description'];
-
-    $checkSql = "SELECT * FROM category WHERE UPPER(Category_name)=UPPER('$name')";
-    $checkResult = $conn->query($checkSql);
-
-    if ($checkResult->rowCount() == 0) {
-        echo "category doesnt exist";
-    } else {
-        $updateSql = "UPDATE type SET Category_name = '$newName', Category_description = '$newDescription' WHERE UPPER(Category_name) = UPPER('$name')";
-        $conn->exec($updateSql);
-
-        echo "Category updated";
     }
-} catch (PDOException $e) {
-    echo "error " . $e->getMessage();
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
 }
+function update()
+{
+
+
+    require '../Connection/connection.php';
+
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+        $name = $_POST['name-update'];
+        $newName = $_POST['name'];
+        $newDescription = $_POST['description'];
+
+        $checkSql = "SELECT * FROM category WHERE UPPER(Category_name)=UPPER('$name')";
+        $checkResult = $conn->query($checkSql);
+
+        if ($checkResult->rowCount() == 0) {
+            echo "category doesnt exist";
+        } else {
+            $updateSql = "UPDATE type SET Category_name = '$newName', Category_description = '$newDescription' WHERE UPPER(Category_name) = UPPER('$name')";
+            $conn->exec($updateSql);
+
+            echo "Category updated";
+        }
+    } catch (PDOException $e) {
+        echo "error " . $e->getMessage();
+    }
 }
 
-function read(){
+function read()
+{
     try {
         require "../Connection/connection.php";
         require "category_class.php";
-        
-        
-        
-        
-        
+
+
+
+
+
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $s="SELECT * FROM category";
-        $r= $conn->query($s);
-        echo"<table>";
+        $s = "SELECT * FROM category";
+        $r = $conn->query($s);
+        echo "<table>";
         echo "<tr>";
-        echo "<th> Category id </th>" ;
-        echo "<th> Category name </th>" ;
-        echo "<th> Category description </th>" ;
+        echo "<th> Category id </th>";
+        echo "<th> Category name </th>";
+        echo "<th> Category description </th>";
         echo "</tr>";
         foreach ($r as $i) {
-            echo"<tr>";
-            echo "<td>".$i['ID_Category']."</td>";
-            echo "<td>".$i['category_name']."</td>";
-            echo "<td>".$i['Category_description']."</td>";
+            echo "<tr>";
+            echo "<td>" . $i['ID_Category'] . "</td>";
+            echo "<td>" . $i['category_name'] . "</td>";
+            echo "<td>" . $i['Category_description'] . "</td>";
             echo "</tr>";
         };
-        echo"</table>";
-        
-        
-        
-        
-        
-        } catch (PDOException $e) 
-        {
-            echo "". $e->getMessage();  
-        }
-
-
+        echo "</table>";
+    } catch (PDOException $e) {
+        echo "" . $e->getMessage();
+    }
 }
 
-function delete(){
+function delete()
+{
 
     try {
         require "../Connection/connection.php";
         require "category_class.php";
-        
+
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-       
+
         $name = $_POST['name'];
-        $t=  "DELETE FROM category WHERE UPPER(Category_name)=UPPER($name)";
+        $t =  "DELETE FROM category WHERE UPPER(Category_name)=UPPER($name)";
         $conn->exec($t);
-          
-        
-        
-        
-        } catch (PDOException $e) 
-        {
-            echo "". $e->getMessage();  
-        }
-
-
+    } catch (PDOException $e) {
+        echo "" . $e->getMessage();
+    }
 }
 
 
-function create() {
+function create()
+{
     try {
         require "../Connection/connection.php";
         require "category_class.php";
-        
+
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-       
-       $name= $_POST['name'];
-       $desc= $_POST['description'];
-    $l="SELECT * FROM category WHERE UPPER(Category_name)=UPPER($name) AND UPPER(Category_description)=UPPER($description)";
-    $r=$conn->query($l);
-    if($r->rowCount()> 0){
-        echo"already used";}
-        else{
-    $l="INSERT INTO category (Category_name,Category_description) VALUES (?,?)";
-    $r=$conn->prepare($l);
-    $r->execute([$name,$description]);
+
+        $name = $_POST['name'];
+        $desc = $_POST['description'];
+        $l = "SELECT * FROM category WHERE UPPER(Category_name)=UPPER($name) AND UPPER(Category_description)=UPPER($description)";
+        $r = $conn->query($l);
+        if ($r->rowCount() > 0) {
+            echo "already used";
+        } else {
+            $l = "INSERT INTO category (Category_name,Category_description) VALUES (?,?)";
+            $r = $conn->prepare($l);
+            $r->execute([$name, $description]);
         }
-    
-    
-    
-        
-        } catch (PDOException $e) 
-        {
-            echo "". $e->getMessage();  
-        }
-
-
-
-
-
-
-
+    } catch (PDOException $e) {
+        echo "" . $e->getMessage();
+    }
 }
-
-
-
-
-
