c4b383d722cf46dcb51d1db26ec291f51e239ddc
walaaghrairi <walaaeddine.ghrairi@esprit.tn>
2023-11-16 23:41:05 +0100

    category_crud

---
+++ model/Category/category_read.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>read</title>
+</head>
+<body>
+<?php
+require "category_class.php";
+read();
+?>
+</body>
+</html>
+
