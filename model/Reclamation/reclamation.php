b1b506bd643793e3b88695c2a52ea96c3f167d93
Abd Razek Nakhli <abdrazek.nakhli@esprit.tn>
2023-11-16 13:35:04 +0100

    commiti wsaybna

--- model/Reclamation/reclamation.php
+++ model/Reclamation/reclamation.php
@@ -2,65 +2,60 @@
 
 require '../Connection/connection.php';
 require_once 'reclamation_class.php';
-class ReclamationCrud {
-   public function ajouter_reclamation($reclamation)
-   {
-    $sql = "INSERT INTO reclamation_tab (ID_Reclamation, Reclamation_text, Reclamation_date, Reclamation_status) VALUES (:ID_Reclamation, :Reclamation_text, :Reclamation_date, :Reclamation_status)";
-    $db = config::getconnexion();
-    try{
-        $query = $db->prepare($sql);
-        $query->execute([
-            'ID_Reclamation'=>$reclamation->getID_reclamation(),
-            'Reclamation_text' => $reclamation->getReclamation_text(),
-            'Reclamation_date' => $reclamation->getReclamation_date()->format('Y-m-d'),
-            'Reclamation_status' => $reclamation->getReclamation_status(),
-        ]);
-    }catch ( Exception $e) {
-        die('Erreur:' . $e->getMessage()); 
+class ReclamationCrud
+{
+    private $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+    public function ajouter_reclamation($reclamation)
+    {
+        $sql = "INSERT INTO reclamation_tab (ID_Reclamation, Reclamation_text, Reclamation_date, Reclamation_status) VALUES (:ID_Reclamation, :Reclamation_text, :Reclamation_date, :Reclamation_status)";
+        try {
+            $query = $this->db->prepare($sql);
+            $query->execute([
+                'ID_Reclamation' => $reclamation->getID_reclamation(),
+                'Reclamation_text' => $reclamation->getReclamation_text(),
+                'Reclamation_date' => $reclamation->getReclamation_date()->format('Y-m-d'),
+                'Reclamation_status' => $reclamation->getReclamation_status(),
+            ]);
+        } catch (Exception $e) {
+            error_log('Error:' . $e->getMessage());
+            throw $e;
+        }
     }
 
-    function afficherReclamation()
+    function afficherReclamation($db)
     {
         $sql = "SELECT * FROM reclamation_tab";
-        $db = config::getConnexion();
+
         try {
             $list = $db->query($sql);
             return $list;
         } catch (Exception $e) {
-            die('Error:' . $e->getMessage());
+            error_log('Error:' . $e->getMessage());
+            throw $e;
         }
     }
 
-    function supprimerReclamation($ID_reclamation)
+    function supprimerReclamation($db, $ID_reclamation)
     {
         $sql = " DELETE FROM reclamation_tab WHERE ID_reclamation=:ID_reclamation";
-        $db = config::getConnexion();
         $req = $db->prepare($sql);
         $req->bindValue(':ID_reclamation', $ID_reclamation);
         try {
             $req->execute();
         } catch (Exception $e) {
-            die('Erreur:' . $e->getMessage());
+            error_log('Erreur:' . $e->getMessage());
+            throw $e;
         }
     }
-
-    function recupererReclamation($ID_reclamation)
-    {
-        $sql = "SELECT * from reclamation_tab where ID_reclamation=$ID_reclamation";
-        $db = config::getConnexion();
-        try {
-            $query = $db->prepare($sql);
-            $query->execute();
-            $reclamation = $query->fetch();
-            return $reclamation;
-        }catch (Exception $e) {
-            die('Erreur:' . $e->getMessage());
-        }
-    }
-
-
-    }   
 }
+$db = new PDO('mysql:host=localhost;dbname=recoverybutterfly', 'root', '');
+$reclamationCRUD = new ReclamationCRUD($db);
+$reclamationCRUD->ajouter_reclamation($reclamation);
 
 
 ?>
