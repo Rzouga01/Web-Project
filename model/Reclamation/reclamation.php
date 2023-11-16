7e1a6016b7aed2af53024a7b0b8de0251ffffef1
Abd Razek Nakhli <abdrazek.nakhli@esprit.tn>
2023-11-16 13:32:21 +0100

    khw yezeha

--- model/Reclamation/reclamation.php
+++ model/Reclamation/reclamation.php
@@ -1,28 +1,23 @@
 <?php
 
 require '../Connection/connection.php';
-require 'reclamation_class.php';
-
-
+require_once 'reclamation_class.php';
 class ReclamationCrud {
    public function ajouter_reclamation($reclamation)
    {
-    $sql = "SELECT * FROM reclamation_tab VALUES (:ID_Reclamation,:Reclamation_text,:Reclamation_date,:Reclamation_status)";
+    $sql = "INSERT INTO reclamation_tab (ID_Reclamation, Reclamation_text, Reclamation_date, Reclamation_status) VALUES (:ID_Reclamation, :Reclamation_text, :Reclamation_date, :Reclamation_status)";
     $db = config::getconnexion();
     try{
         $query = $db->prepare($sql);
         $query->execute([
-            'ID_Reclamation'=>$ID_reclamation->getID_reclamation(),
-            'Reclamation_text' => $reclamation_text->getReclamation_text(),
-            'Reclamation_date' => $reclamation_date->getReclamation_date()->format('Y-m-d'),
-            'Reclamation_status' => $status_reclamation->getReclamation_status(),
+            'ID_Reclamation'=>$reclamation->getID_reclamation(),
+            'Reclamation_text' => $reclamation->getReclamation_text(),
+            'Reclamation_date' => $reclamation->getReclamation_date()->format('Y-m-d'),
+            'Reclamation_status' => $reclamation->getReclamation_status(),
         ]);
     }catch ( Exception $e) {
         die('Erreur:' . $e->getMessage()); 
     }
-     
-
-
 
     function afficherReclamation()
     {
