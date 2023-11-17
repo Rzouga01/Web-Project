ef7d4a97d5beacd9b23698faab5131de54abc555
Abd Razek Nakhli <abdrazek.nakhli@esprit.tn>
2023-11-17 02:46:25 +0100

    crud complete

--- model/Connection/connection.php
+++ model/Connection/connection.php
@@ -6,7 +6,6 @@ $password = '';
 $dbname = 'recoverybutterfly';
 
 try {
-
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
--- model/User/user.php
+++ model/User/user.php
@@ -1,32 +1,58 @@
 <?php
+require_once __DIR__ . '/../Connection/connection.php';
 require_once 'user_class.php';
-require_once '../../model/Connection/connection.php';
 
-class UserCRUD {
+class UserCRUD
+{
     private $db;
 
-    public function __construct($db) {
+    public function __construct($db)
+    {
         $this->db = $db;
     }
 
-    public function createUser(UserClass $user) {
-        $rq = $this->db->prepare("INSERT INTO user (ID_USER, First_Name, Last_Name, Email, Phone_number, Password, Birthdate, Country, Role) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $rq->execute([$user->getID_USER(), $user->getFirst_Name(), $user->getLast_Name(), $user->getEmail(), $user->getPhone_number(), $user->getPassword(), $user->getBirthdate(), $user->getCountry(), $user->getRole()]);
-    }   
-}
+    public function createUser(UserClass $user)
+    {
+        $insert = $this->db->prepare("INSERT INTO user (ID_USER, First_Name, Last_Name, Password, Phone_number, Birthdate, Country, Email, Role) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $result = $insert->execute([
+            $user->getID_USER(),
+            $user->getFirst_Name(),
+            $user->getLast_Name(),
+            $user->getPassword(),
+            $user->getPhone_number(),
+            $user->getBirthdate(),
+            $user->getCountry(),
+            $user->getEmail(),
+            $user->getRole()
+        ]);
+        return $result;
+    }
+    public function getAllUsers()
+    {
+        $select = $this->db->prepare("SELECT * FROM user");
+        $select->execute();
+        $users = $select->fetchAll(PDO::FETCH_ASSOC);
+        return $users;
+    }
+    public function updateUser($user)
+    {
+        $query = $this->db->prepare("UPDATE user SET First_Name = ?, Last_Name = ?, Email = ?, Phone_number = ?, Password = ?, Birthdate = ?, Country = ?, Role = ? WHERE ID_USER = ?");
+        $query->execute([$user->getFirstName(), $user->getLastName(), $user->getEmail(), $user->getPhoneNumber(), $user->getPassword(), $user->getBirthdate(), $user->getCountry(), $user->getRole(), $user->getId()]);
+    }
 
+    public function deleteUser($id)
+    {
+        $query = $this->db->prepare("DELETE FROM user WHERE ID_USER = ?");
+        $query->execute([$id]);
+    }
+}
+$userCRUD = new UserCRUD($pdo);
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (
-        isset($_POST["ID_USER"]) &&
-        isset($_POST["First_Name"]) &&
-        isset($_POST["Last_Name"]) &&
-        isset($_POST["Email"]) &&
-        isset($_POST["Phone_number"]) &&
-        isset($_POST["Password"]) &&
-        isset($_POST["Birthdate"]) &&
-        isset($_POST["Country"]) &&
-        isset($_POST["Role"])
-    ) {
+
+
+    if (isset($_POST["delete"])) {
+        $userCRUD->deleteUser($_POST["delete"]);
+    } elseif (isset($_POST["update"])) {
         $user = new UserClass(
             $_POST["ID_USER"],
             $_POST["First_Name"],
@@ -38,12 +64,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_POST["Country"],
             $_POST["Role"]
         );
-
-        $db = new PDO('mysql:host=localhost;dbname=recoverybutterfly', 'root', ''); 
-        $userCRUD = new UserCRUD($db);
+        $userCRUD->updateUser($user);
+    } else {
+        $id = uniqid(rand(1, 1000));
+        $user = new UserClass(
+            $id,
+            $_POST["First_Name"],
+            $_POST["Last_Name"],
+            $_POST["Email"],
+            $_POST["Phone_number"],
+            $_POST["Password"],
+            $_POST["Birthdate"],
+            $_POST["Country"],
+            $_POST["Role"]
+        );
         $userCRUD->createUser($user);
     }
 }
-
-?>
-
