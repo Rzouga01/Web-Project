a26d9c2245c9f49c8975fed8d4784771d0625eb2
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2023-11-18 15:21:25 +0100

    5demt reclamation 5edma mouch véridié

---
+++ database/config.php
@@ -0,0 +1,27 @@
+<?php
+
+class Config
+{
+    private static $pdo = null;
+
+    private static $servername = 'localhost';
+    private static $username = 'root';
+    private static $password = '';
+    private static $dbname = 'recoverybutterfly';
+
+    public static function getConnexion()
+    {
+        if (!isset(self::$pdo)) {
+            try {
+                self::$pdo = new PDO("mysql:host=" . self::$servername . ";dbname=" . self::$dbname, self::$username, self::$password, [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                ]);
+            } catch (Exception $e) {
+                die('Erreur: ' . $e->getMessage());
+            }
+        }
+        return self::$pdo;
+    }
+}
+?>
