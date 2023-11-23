92325aba5ccb700f9128f3a0934000a75f949866
AtfastrSlushyMaker <atfastr@gmail.com>
2023-11-23 12:41:16 +0100

    Update link in dashboard_type.php

--- view/Type/dashboard_type.php
+++ view/Type/dashboard_type.php
@@ -43,7 +43,7 @@ require "../../controller/Type/typeC.php";
                         <i class="fa fa-database"></i>
                         <span class="nav-item">Project</span>
                     </a></li>
-                <li><a href="dashboard_type.php">
+                <li><a href="../Reclamation/dashboard_reclamation.php">
                         <i class="fa fa-exclamation-triangle"></i>
                         <span class="nav-item">Reclamation</span>
                     </a></li>
