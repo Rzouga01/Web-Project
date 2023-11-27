67107b73ab08f7840e8b3e8c75e4604b9d96f736
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2023-11-27 13:31:46 +0100

    aaaa

--- view/Reclamation/dashboard_reclamation.php
+++ view/Reclamation/dashboard_reclamation.php
@@ -43,7 +43,7 @@ require_once "../../model/Reclamation/reclamationC.php";
                         <i class="fa fa-database"></i>
                         <span class="nav-item">Project</span>
                     </a></li>
-                <li><a href="../Reclamation/dashboard_reclamation.php">
+                <li><a href="dashboard_reclamation.php">
                         <i class="fa fa-exclamation-triangle"></i>
                         <span class="nav-item">Reclamation</span>
                     </a></li>
