27a0d9d568ce97ffa92bc3970d55fc18f60f09b5
Abd Razek Nakhli <abdrazek.nakhli@esprit.tn>
2023-11-30 23:54:52 +0100

    User session ok (ne9sa  trouchik mouhem tekhdm)

---
+++ controller/User/login.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    require_once '../../database/connect.php'; 
+    require_once '../../controller/User/user.php'; 
+    require_once '../../model/User/userC.php'; 
+
+    $user = new UserCRUD();
+
+    if (isset($_POST["email"]) && isset($_POST["password"])) {
+        $email = $_POST["email"];
+        $userData = $user->getUserByEmail($email);
+        
+        if ($userData == null) {
+            echo "<script>alert('Email not found in the database!'); window.location.href='../../view/User/user.html#signin';</script>";
+            exit;
+        }
+
+        if (isset($userData)&&$userData['Email']==$email) {
+            $_SESSION['user_id'] = $userData['id'];
+            $_SESSION['username'] = $userData['Email'];
+            $_SESSION['password'] = $userData['password'];
+            header('Location: ../../view/index.php');
+            exit;
+        } else {
+            echo "<script>alert('Invalid email or password!'); window.location.href='../../view/User/user.html#signin';</script>";
+        }
+    } else {
+        echo "<script>alert('Please provide both email and password!'); window.location.href='../../view/User/user.html#signin';</script>";
+    }
+} else {
+    echo "<script>alert('Invalid request method!'); window.location.href='../../view/User/user.html#signin';</script>";
+}
+?>
--- view/index.php
+++ view/index.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+$isLoggedIn = isset($_SESSION['username']);
+
+if ($isLoggedIn) {
+	$username = $_SESSION['username'];
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -83,15 +93,27 @@
 											href="#scrollspyHeading5">Products</a></li>
 								</ul>
 							</li>
-							<li class="nav-item">
-								<a class="btn btn-primary btn-lg rounded-pill" href="User/user.html#signup">Join Us</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link me-4" href="User/user.html#signin">Login</a>
-							</li>
+							<?php if ($isLoggedIn) { ?>
+								<li class="nav-item dropdown">
+									<a class="nav-link dropdown-toggle me-4" href="#" id="navbarDropdown" role="button"
+										data-bs-toggle="dropdown" aria-expanded="false">
+										<?php echo $username; ?>
+									</a>
+									<ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+										<li><a href="../controller/User/logout.php" class="dropdown-item">Logout</a></li>
+									</ul>
+								</li>
+							<?php } else { ?>
+								<li class="nav-item">
+									<a class="btn btn-primary btn-lg rounded-pill" href="User/user.html#signup">Join Us</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link me-4" href="User/user.html#signin">Login</a>
+								</li>
+							<?php } ?>
 						</ul>
-
 					</div>
+
 				</div>
 
 			</div>
