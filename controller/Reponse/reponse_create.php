155b606c44a96365be3be79278f96be2e030d5bf
AtfastrSlushyMaker <atfastr@gmail.com>
2023-11-30 21:53:18 +0100

    Fix SQL queries and class names in response files

--- controller/Reponse/reponse_create.php
+++ controller/Reponse/reponse_create.php
@@ -1,19 +1,14 @@
 <?php
-require_once 'reponse.php';
+require_once 'reponse.php'; // Assuming this is your Response class definition
+require_once 'reponseC.php'; // Assuming this is your ResponseC class definition
 
+// Assuming $_POST['ID_Reclamation'] and $_POST['text'] are set
+$RepC = new ResponseC(); // Corrected to use ResponseC instead of Response
+$Rep = new Response($_POST['ID_Reclamation'], $_POST['text']); // Assuming Response is your entity class
 
-
-
-
-$RepC = new Response($_POST['ID_Reclamation'], $_POST['text']);
-
-$Rep = new ResponseC();
-
-$result = $Rep->ajouterResponse($RepC);
+$result = $RepC->ajouterResponse($Rep); // Corrected to use $RepC instead of $Rep
 
 echo $result;
-
-
 
 header('Location: ../../View/Reponse/dashboard_reponse.php');
 exit;
