acc41caf608024853bda7fce48fde1c40d51e7d6
AtfastrSlushyMaker <atfastr@gmail.com>
2023-12-14 17:58:06 +0100

    Fix navigation link in dashboard_project.php

--- view/Project/dashboard_project.php
+++ view/Project/dashboard_project.php
@@ -106,6 +106,7 @@ require_once "../../controller/Type/TypeC.php";
                         <i class="fas fa-home"></i>
                         <span class="nav-item">Home</span>
                     </a></li>
+
                 <li><a href="../User/dashboard_admin.php?showProfile=true.php">
                         <i class="fas fa-user"></i>
                         <span class="nav-item">Profile</span>
